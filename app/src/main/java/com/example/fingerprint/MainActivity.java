c3bf24bee15226b4980552bed86ed69aa2f7f535
teffa <tefihvmoonwalker77746@gmail.com>
2020-04-07 21:55:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fingerprint/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.fingerprint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import android.app.Person;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView msg_txt = findViewById(R.id.txt_msg);
+        TextView login_btn = findViewById(R.id.login_btn);
+
+        final BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                msg_txt.setText("You can use the fingerprint sensor to Login!");
+                msg_txt.setTextColor(Color.parseColor("#fafafa"));
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                msg_txt.setText("the device don't have a fingerprint sesnsor");
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                msg_txt.setText("the biometric sensors is currrently unvailable");
+                login_btn.setVisibility(View.GONE);
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                msg_txt.setText("your device don't have any fingerprint saved, please check your securty settings");
+                login_btn.setVisibility(View.GONE);
+                break;
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback(){
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+
+
+        });
+
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setDescription("Use your fingerprint to login to your app")
+                .setNegativeButtonText("Cancel")
+                .build();
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+    }
+}
